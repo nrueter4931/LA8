f0233fa9d064d599012e81be6b07aab7a22e6479
Nick Rueter <masternick4931@mail.fresnostate.edu>
2019-12-14 10:37:09 -0800

    imported Toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ToastModule } from './toast/toast.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
